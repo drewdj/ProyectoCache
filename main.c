5c3de989dde885fe1dd0cd1074f66f155693e730
drewdj <43813535+drewdj@users.noreply.github.com>
2020-12-07 17:45:22 +0100

    Main files

--- main.c
+++ main.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 
+typedef struct {
+    short int ETQ;
+    short int Datos[8];
+} T_LINEA_CACHE;
+
+
 int main() {
-    printf("Hello, World!\n");
+int tiempoglobal = 0;
+int numfallos = 0;
     return 0;
 }
