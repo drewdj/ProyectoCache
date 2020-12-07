9afeb89d30943b9e8ab88aadb0973c2fe083d91b
drewdj <43813535+drewdj@users.noreply.github.com>
2020-12-07 19:58:03 +0100

    data initialization

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 
 typedef struct {
     short int ETQ;
@@ -9,5 +10,17 @@ typedef struct {
 int main() {
 int tiempoglobal = 0;
 int numfallos = 0;
+FILE *fp;
+
+fp = fopen("E:\\ProyectoCache\\accesos_memoria.txt","r");
+
+    for (int i = 0; i < 4; ++i) {
+        T_LINEA_CACHE linea[i];
+        linea[i].ETQ = 0xFF;
+        for (int j = 0; j < 8; ++j) {
+            linea[i].Datos[j]=0;
+        }
+    }
+
     return 0;
 }
