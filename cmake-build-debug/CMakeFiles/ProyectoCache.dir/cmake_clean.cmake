de9c2c3b4d9342dc088482cad588bfa79de8e9a1
drewdj <43813535+drewdj@users.noreply.github.com>
2020-12-07 17:36:22 +0100

    First commit

---
+++ cmake-build-debug/CMakeFiles/ProyectoCache.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ProyectoCache.dir/main.c.obj"
+  "ProyectoCache.exe"
+  "ProyectoCache.exe.manifest"
+  "ProyectoCache.pdb"
+  "libProyectoCache.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/ProyectoCache.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/ProyectoCache.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for ProyectoCache.
+# This may be replaced when dependencies are built.
