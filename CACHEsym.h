fbfeef22d7198eb82189296625590f0f55d205ff
Hao Long <longeliu1333@gmail.com>
2020-12-12 22:28:32 +0100

    CacheBeta

---
+++ CACHEsym.h
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+#define RAM_SIZE 1024
+typedef struct {
+    short int ETQ;
+    short int Datos[8];
+} T_LINEA_CACHE;
+void hexToBin(char * hex,char * accesoArray);
+int comprobarETQ(char * ETQ,char * bloque,T_LINEA_CACHE * linea);
+
+void actualizadorCache(char * accesoBinario, T_LINEA_CACHE * linea, unsigned char * RAM);
+int binToDec(char * bin);
+char lectorAcceso(char * accesoBinario, T_LINEA_CACHE * cache);
+void actualizadorTexto(char * texto, char palabra, int posicion /*Contador de iteraciones de accesos*/);
