8a8ea7adb0213a1562244b1faaf0a51636c0f7d5
Hao Long <longeliu1333@gmail.com>
2020-12-12 20:24:47 +0100

    actualizacion main

--- main.c
+++ main.c
@@ -17,7 +17,11 @@ int main() {
   fRAM = fopen("RAM.bin", "rb");
   unsigned char RAM[RAM_SIZE];
   fread(RAM ,sizeof(RAM), 1 , fRAM);
-  printf("%s\n", RAM);
+  if (fRAM == NULL) {
+    printf("No se ha podido abrir RAM.bin");
+    return 0;
+  }
+  printf("%s\n", RAM);//Quitar al final, es un control
   fclose(fRAM);//ALMACENA 5 caracteres de más comprobar mañana
   //Cargar Accesos a memoria
   fp = fopen("accesos_memoria.txt","r");
@@ -40,7 +44,17 @@ int main() {
   /*leer cada acceso de linea, como no podemos guardar en otro array o en el
   mismo array todos los accesos, sino de linea en linea, lo suyo sería trabajar con los accesos con el puntero de FILE*/
   while (fgets(arrayAccesos, MAXCHAR, fp) != NULL){
-    printf("%s", arrayAccesos);
+    char accesoBinario[10] = hexToBin(arrayAccesos);
+    char etqBin[5] = {accesoBinario[0], accesoBinario[1], accesoBinario[2], accesoBinario[3], accesoBinario[4]};
+    char bloqueBin[2] = {accesoBinario[5], accesoBinario[6]}
+    char palabraBin[3] = {accesoBinario[7], accesoBinario[8], accesoBinario[9]}
+    int decEtq = binToDec(etqBin);
+    int decBlock = binToDec(bloqueBin);
+    int decPalabra = binToDec(palabraBin);
+    int tf = comprobarETQ(accesoBinario);
+    if (tf == 1){
+      printf("Acierto de CACHE ADDR %s, ETQ %d linea 0%d palabra 0%d DATO %x", arrayAccesos, );
+    }
   }
   fclose(fp);
   return 0;
